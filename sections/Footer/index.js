a34a6757f80f263250b5f413124a2d7d44c472db
ignaguri <ignacioguri@gmail.com>
2022-11-06 17:32:02 +0100

    remove google tag tracking

--- sections/Footer/index.js
+++ sections/Footer/index.js
@@ -1,19 +1,8 @@
-import * as gtag from '../../lib/gtag';
-
 export default function Footer() {
-  const handleGoToGithub = () => {
-    gtag.event({
-      action: 'go_to_github',
-      category: 'Contact',
-      label: 'Footer',
-    });
-  };
-
   return (
     <footer className="w-full h-24 flex justify-center items-center border-t border-solid border-gray-300">
       <a
         className="flex justify-center items-center text-black no-underline"
-        onClick={handleGoToGithub}
         href="https://github.com/ignaguri"
         rel="noopener noreferrer"
       >
