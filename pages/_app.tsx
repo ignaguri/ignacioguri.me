3d90794d9a03e77403114984ec7097105089ab5a
Ignacio Guri <ignacio.guri@doctari.de>
2023-09-05 10:50:00 +0200

    feat: add Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 import "../styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
