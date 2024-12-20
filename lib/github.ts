a4b729d0f53c181c487349905a6a7dc477d69c04
Ignacio Guri <ignacio.guri@holidu.com>
2024-12-20 15:52:25 +0100

    remove try/catch in fetchGitHubProjects

--- lib/github.ts
+++ lib/github.ts
@@ -27,74 +27,70 @@ export async function fetchGitHubProjects(username: string) {
     return cache[cacheKey].data;
   }
 
-  try {
-    const { data: repos }: { data: ListForUserResponse } =
-      await octokit.rest.repos.listForUser({
-        type: "owner",
-        username,
-      });
+  const { data: repos }: { data: ListForUserResponse } =
+    await octokit.rest.repos.listForUser({
+      type: "owner",
+      username,
+    });
 
-    const projects = await Promise.all(
-      repos.map(async (repo) => {
-        if (repo.fork || repo.archived || repo.private) {
-          return [];
-        }
+  const projects = await Promise.all(
+    repos.map(async (repo) => {
+      if (repo.fork || repo.archived || repo.private) {
+        return [];
+      }
 
-        const { data: languagesData }: { data: ListLanguagesResponse } =
-          await octokit.rest.repos.listLanguages({
+      const { data: languagesData }: { data: ListLanguagesResponse } =
+        await octokit.rest.repos.listLanguages({
+          owner: username,
+          repo: repo.name,
+        });
+      const techs = Object.keys(languagesData);
+
+      // Fetch the first page of commits to get the total count
+      const { data: commitsData, headers } =
+        await octokit.rest.repos.listCommits({
+          owner: username,
+          per_page: 10,
+          repo: repo.name,
+        });
+
+      let commitCount = commitsData.length;
+
+      if (commitCount === 10 && headers.link) {
+        const lastPageMatch = headers.link.match(/&page=(\d+)>; rel="last"/);
+        if (lastPageMatch) {
+          const lastPage = parseInt(lastPageMatch[1], 10);
+          const lastPageCommits = await octokit.rest.repos.listCommits({
             owner: username,
-            repo: repo.name,
-          });
-        const techs = Object.keys(languagesData);
-
-        // Fetch the first page of commits to get the total count
-        const { data: commitsData, headers } =
-          await octokit.rest.repos.listCommits({
-            owner: username,
+            page: lastPage,
             per_page: 10,
             repo: repo.name,
           });
-
-        let commitCount = commitsData.length;
-
-        if (commitCount === 10 && headers.link) {
-          const lastPageMatch = headers.link.match(/&page=(\d+)>; rel="last"/);
-          if (lastPageMatch) {
-            const lastPage = parseInt(lastPageMatch[1], 10);
-            const lastPageCommits = await octokit.rest.repos.listCommits({
-              owner: username,
-              page: lastPage,
-              per_page: 10,
-              repo: repo.name,
-            });
-            commitCount = (lastPage - 1) * 10 + lastPageCommits.data.length;
-          }
+          commitCount = (lastPage - 1) * 10 + lastPageCommits.data.length;
         }
+      }
 
-        return {
-          commitCount,
-          description: repo.description,
-          id: repo.id,
-          link: repo.homepage ?? null,
-          name: repo.name,
-          repo: repo.html_url,
-          techs,
-        };
-      })
-    );
+      return {
+        commitCount,
+        description: repo.description,
+        id: repo.id,
+        link: repo.homepage ?? null,
+        name: repo.name,
+        repo: repo.html_url,
+        techs,
+      };
+    })
+  );
 
-    const topProjects = projects
-      .flat()
-      .sort((a, b) => b.commitCount - a.commitCount);
+  const topProjects = projects
+    .flat()
+    .sort((a, b) => b.commitCount - a.commitCount);
 
-    // Cache the result
-    cache[cacheKey] = {
-      data: topProjects,
-      expiry: now + CACHE_DURATION,
-    };
+  // Cache the result
+  cache[cacheKey] = {
+    data: topProjects,
+    expiry: now + CACHE_DURATION,
+  };
 
-    return topProjects;
-  } catch (error) {
-    throw error;
-  }
+  return topProjects;
 }
